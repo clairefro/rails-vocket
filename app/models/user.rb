5ffa1a948c6e6d78847bf2f6e70b5c5bf40bed09
claire froelich <claire.froelich@gmail.com>
2020-01-27 21:48:44 -0500

    update associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :vocabs
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
--- app/models/vocab.rb
+++ app/models/vocab.rb
@@ -1,3 +1,4 @@
 class Vocab < ApplicationRecord
   belongs_to :user
+  has_many :tags
 end
